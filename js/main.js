b8285dfb27f1ddf208b2cb016811a28ba9518888
ofershi <68652453+ofershi@users.noreply.github.com>
2022-12-13 12:49:32 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,64 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("find-me").addEventListener("click", geoFindMe);
+    document.getElementById("shareBtn").addEventListener("click", share);
+    
+})
+
+let googleURL;
+
+    function geoFindMe()
+    {
+          const status = document.querySelector('#status');
+          const mapLink = document.querySelector('#map-link');
+          mapLink.href = '';
+          mapLink.textContent = '';
+    
+            if (!navigator.geolocation) {
+            status.textContent = 'Geolocation is not supported by your browser';
+          } else {
+            status.textContent = 'Locating…';
+            navigator.geolocation.getCurrentPosition(success, error);
+          }
+        
+          function success(position) {
+            const latitude  = position.coords.latitude;
+            const longitude = position.coords.longitude;
+        
+            status.textContent = '';
+            mapLink.href = `https://maps.google.com/?q=${latitude},${longitude}`;
+            googleURL = `https://maps.google.com/?q=${latitude},${longitude}`; 
+            mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+            iframe.src = `https://maps.google.com/?output=embed&q=${latitude},${longitude}`
+            iframe.classList.remove("d-none")
+          }
+  
+        
+          function error() {
+            status.textContent = 'Unable to retrieve your location';
+          }
+        
+        }
+        
+       
+function share(){
+
+    const shareData = {
+        title: 'MDN',
+        text: "קו רוחב הוא",
+        url: googleURL
+      }
+      
+      const btn = document.querySelector('#shareBtn');
+      //const resultPara = document.querySelector('.result');
+      
+      // Share must be triggered by "user activation"
+      btn.addEventListener('click', async () => {
+        try {
+          await navigator.share(shareData);
+          //resultPara.textContent = 'MDN shared successfully';
+        } catch (err) {
+          //resultPara.textContent = `Error: ${err}`;
+        }
+      });
+
+}
